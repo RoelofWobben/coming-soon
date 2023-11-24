bec12155f1d5ec11def26de661649057563401f5
Noname <noname@example.com>
2023-11-24 12:27:25 +0100

    first steps

---
+++ roelof.js
@@ -0,0 +1,22 @@
+console.log('js werkt'); 
+/*
+const validateEmailInput = () => {
+    
+    var div = document.querySelector('.email');
+    var input_box = document.querySelector('.mail'); 
+    var icon = div.querySelector('.icon_error')
+    var input = document.querySelector('.mail').value;
+    var error_message = div.querySelector('.error');
+        
+    // validate input if it is a valid email adress
+
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!input.match(validRegex)){
+        error_message.innerHTML = "Looks like this is not a valid email address"
+    }
+    
+    displayErrorMessage(input.match(validRegex), error_message , input_box, icon); 
+}
+
+*/ 
